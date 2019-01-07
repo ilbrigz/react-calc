2c5d8ae1e24400cd3ae4aaf58b8268b0d51baf8d
Brayn Brigoli <32309779+ilbrigz@users.noreply.github.com>
2019-01-07 15:12:06 +0800

    completed styling.
    TODO: animation

--- src/index.js
+++ src/index.js
@@ -8,18 +8,11 @@ class App extends React.Component {
     return (
       <div className="App">
         <div className="calculator">
-          <div className="screen">screen</div>
+          <div className="screen">
+            <p>1+1</p>
+            <p>2</p>
+          </div>
           <div className="keys">
-            <button className="key one">1</button>
-            <button className="key two">2</button>
-            <button className="key three">3</button>
-            <button className="key four">4</button>
-            <button className="key five">5</button>
-            <button className="key six">6</button>
-            <button className="key seven">7</button>
-            <button className="key eight">8</button>
-            <button className="key nine">9</button>
-            <button className="key zero">0</button>
             <button className="key decimal">.</button>
             <button className="key times">x</button>
             <button className="key plus">+</button>
@@ -27,6 +20,17 @@ class App extends React.Component {
             <button className="key divide">/</button>
             <button className="key back">Back</button>
             <button className="key c">C</button>
+            <button className="key nine">9</button>
+            <button className="key eight">8</button>
+            <button className="key seven">7</button>
+            <button className="key six">6</button>
+            <button className="key five">5</button>
+            <button className="key four">4</button>
+            <button className="key three">3</button>
+            <button className="key two">2</button>
+            <button className="key one">1</button>
+            <button className="key zero">0</button>
+
             <button
               className="key equals"
               style={{ backgroundColor: "#F15A2B" }}
